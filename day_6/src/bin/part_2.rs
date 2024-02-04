48fc079e3767271c4125bf1cf4dd9e2b49ca4d93
s-jad <s_jad@web.de>
2024-02-04 05:15:48 +0100

    Solved day 6 part 2

---
+++ day_6/src/bin/part_2.rs
@@ -0,0 +1,83 @@
+use itertools::Itertools;
+use std::time::Instant;
+
+fn control_lights<'a>(grid: &mut [[u8; 1000]; 1000], op: &'a str) {
+    let parts = op.split_terminator(&[',', ' ', '\n']).collect_vec();
+    match parts[0] {
+        "turn" => {
+            let (sx, sy, ex, ey) = parts[2..]
+                .iter()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect_tuple::<(_, _, _, _)>()
+                .unwrap();
+
+            match parts[1] {
+                "on" => {
+                    for y in sy..=ey {
+                        for x in sx..=ex {
+                            grid[y][x] += 1;
+                        }
+                    }
+                }
+                "off" => {
+                    for y in sy..=ey {
+                        for x in sx..=ex {
+                            grid[y][x] -= if grid[y][x] == 0 { 0 } else { 1 };
+                        }
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        "toggle" => {
+            let (sx, sy, ex, ey) = parts[1..]
+                .iter()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect_tuple::<(_, _, _, _)>()
+                .unwrap();
+
+            for y in sy..=ey {
+                for x in sx..=ex {
+                    grid[y][x] += 2;
+                }
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn process(input: &str) -> usize {
+    let ops = input.lines().collect_vec();
+    let mut grid = [[0u8; 1000]; 1000];
+
+    for op in ops {
+        control_lights(&mut grid, op);
+    }
+
+    grid.iter()
+        .map(|row| row.iter().map(|l| *l as usize).sum::<usize>())
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
