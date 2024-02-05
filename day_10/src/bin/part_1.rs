35c7b9746770e2d799d28e082eef420f30ce9122
s-jad <s_jad@web.de>
2024-02-05 14:01:05 +0100

    Solved day 10 part 1

---
+++ day_10/src/bin/part_1.rs
@@ -0,0 +1,51 @@
+use itertools::Itertools;
+use std::time::Instant;
+
+fn process() -> usize {
+    let mut input: Vec<u8> = vec![1, 1, 1, 3, 1, 2, 2, 1, 1, 3];
+
+    for _ in 0..40 {
+        let mut next: Vec<u8> = Vec::new();
+        let mut idx = 0;
+
+        while idx < input.len() {
+            let num = input[idx];
+            let mut count = 1;
+            idx += 1;
+
+            if idx < input.len() {
+                while input[idx] == num {
+                    count += 1;
+                    idx += 1;
+                }
+            }
+            next.push(count);
+            next.push(num);
+        }
+
+        input = next;
+    }
+
+    input.len()
+}
+
+fn main() {
+    let start = Instant::now();
+    let output = process();
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
